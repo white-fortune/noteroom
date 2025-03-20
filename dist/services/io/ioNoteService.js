4a392d8fea76acbf0200eb28c4ea084b8daa54f8
ju4700 <jalal.dev.design@gmail.com>
2025-03-21 03:27:18 +0600

    refactor: remove unused type definitions and service implementations for cleaner codebase

--- dist/services/io/ioNoteService.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = noteIOHandler;
-function noteIOHandler(io, socket) {
-    socket.on('join-room', (room) => {
-        socket.join(room);
-    });
-}
